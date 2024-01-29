bb9d110d83f26ded9e1bbfa17d9f2d3f7dd8ca10
BrianFugate <fugateb@yahoo.com>
2024-01-29 08:38:31 -0600

    Pushing current CommentsList.jsx for baseline fallback. Recursive function is working as intended, need to filter out repeat <a> tags in result.

--- src/components/CommentsList/CommentsList.jsx
+++ src/components/CommentsList/CommentsList.jsx
@@ -1,33 +1,66 @@
 import React from "react";
 import styles from "./CommentsList.module.css";
-import comments from "../../api/Reddit/comments.json";
+import smallComments from "../../api/Reddit/smallComments.json";
+
+
+// temporary function for debugging, will be removed
+function stringify(obj) {
+    let cache = [];
+    let str = JSON.stringify(obj, function(key, value) {
+      if (typeof value === "object" && value !== null) {
+        if (cache.indexOf(value) !== -1) {
+          // Circular reference found, discard key
+          return;
+        }
+        // Store value in our collection
+        cache.push(value);
+      }
+      return value;
+    });
+    cache = null; // reset the cache
+    return str;
+}
 
 // Recursive function to iterate through comment data and put them
-// in the corrent hierarchy for display
+// in the correct hierarchy for display
 function iterateComments(arr) {
-    return arr.map((element) => {
-        let moreComments;
+    return arr.flatMap((element) => {
+        let moreComments = [];
+        const aTag = (
+            <div className={styles.commentDiv} key={element.data.id}>
+                <a
+                    href={'https://redd.it/' + '1abr3ky'}
+                    target='_blank'
+                    className={styles.comment}
+                    style={{ marginLeft: `${element.data.depth}em` }}
+                >Read 'deep' comments on Reddit
+                </a>
+            </div>
+        );
+        const pTag = (
+            <div className={styles.commentDiv} key={element.data.id}>
+                <p className={styles.comment} style={{ marginLeft: `${element.data.depth}em` }}>{element.data.body}</p>
+            </div>
+        )
 
-        if (Object.hasOwn(element.data, 'replies')) {
-            if (Object.hasOwn(element.data.replies, 'data')) {
-                if (Object.hasOwn(element.data.replies.data, 'children')) {
-                    moreComments = iterateComments(element.data.replies.data.children);            
-                }
-            }                
+        // If current element has a child comment call iterateComments recursively
+        if (element?.data?.replies?.data?.children !== undefined) {
+            moreComments = iterateComments(element.data.replies.data.children);
         }
 
-        return [(
-            <div className={styles.commentDiv} key={element.data.id}>
-                <p className={styles.comment} style={{marginLeft: `${element.data.depth}em`}}>{element.data.body}</p>
-            </div>
-        ), moreComments];
+        // Return link to Reddit if element is not a comment
+        if (element.data.body === undefined) {
+            return [aTag, ...moreComments];
+        } else {
+            return [pTag, ...moreComments];
+        }
     });
 };
 
 export default function CommentsList() {
     return (
         <>
-            {iterateComments(comments[1].data.children)}
+            {iterateComments(smallComments[1].data.children)}
             {/*<div className={styles.line1}></div>*/}
         </>
     );
