bd8f18f6d4f11ead40babf7a3f0ca46b62183f4e
BrianFugate <fugateb@yahoo.com>
2024-01-30 16:22:34 -0600

    Removing links to Reddit from inside comment list. Adding single link at end of each page.

--- src/components/CommentsList/CommentsList.jsx
+++ src/components/CommentsList/CommentsList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./CommentsList.module.css";
-import { Util } from "../../utilities/Util.js";
 import { useSelector } from "react-redux";
 import { selectViewPost } from "../../pages/ViewPost/viewPostSlice.js";
 
@@ -10,17 +9,6 @@ import { selectViewPost } from "../../pages/ViewPost/viewPostSlice.js";
 function iterateComments(arr, id) {
     return arr.flatMap((element) => {
         let moreComments = [];
-        const aTag = (
-            <div className={styles.commentDiv} key={element.data.id}>
-                <a
-                    href={'https://redd.it/' + id}
-                    target='_blank'
-                    className={styles.comment}
-                    style={{ marginLeft: `${element.data.depth}em` }}
-                >Read 'deep' comments on Reddit
-                </a>
-            </div>
-        );
         const pTag = (
             <div className={styles.commentDiv} key={element.data.id}>
                 <p className={styles.comment} style={{ marginLeft: `${element.data.depth}em` }}>{element.data.body}</p>
@@ -32,28 +20,13 @@ function iterateComments(arr, id) {
             moreComments = iterateComments(element.data.replies.data.children, id);
         }
 
-        // Return link to Reddit if element is not a comment
+        // Return recursive data and next comment if it exists
         if (element.data.body === undefined) {
-            return [aTag, ...moreComments];
+            return [...moreComments];
         } else {
             return [pTag, ...moreComments];
         }
     });
-};
-
-// Function to remove repeated links back to Reddit from comments
-function parseComments(arr, id) {
-    const fatArr = iterateComments(arr, id);
-    const parsedFatArr = JSON.parse(Util.stringify(fatArr));
-    const slimArr = [];
-
-    for (let i = 0; i < fatArr.length; i++) {          
-        if (i === 0 || parsedFatArr[i].props.children.type === 'p' || parsedFatArr[i - 1].props.children.type !== 'a') {
-            slimArr.push(fatArr[i]);
-        };
-    };
-
-    return slimArr;
 };
 
 export default function CommentsList() {
@@ -62,8 +35,16 @@ export default function CommentsList() {
 
     return (
         <>
-            {parseComments(comments[1].data.children, id)}
-            {/*<div className={styles.line1}></div>*/}
+            {iterateComments(comments[1].data.children, id)}
+            <div>
+                <a>Load more comments</a>
+                <a
+                    href={'https://redd.it/' + id}
+                    target='_blank'
+                    className={styles.comment}
+                >Read deep nested comments on Reddit
+                </a>
+            </div>
         </>
     );
 };
